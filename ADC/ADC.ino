89a5a9c806a95154f4127126189d733e668ae386
AlexBaturo <mifi123456@mail.ru>
2020-03-25 01:49:27 +0300

    Ацп с прерыванием

--- ADC/ADC.ino
+++ ADC/ADC.ino
@@ -1,23 +1,29 @@
+unsigned int adc_value;
+int high_adc=0,low_adc=0; 
+ 
  void ADC_Init(void)
 
 {
   // Разрешение использования АЦП
-  ADCSRA |= (1<<ADEN);
-  //Делитель 128 = 64 кГц
-  //|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
-  //Внутренний Источник ОН 2,56в, вход ADC3
-  ADMUX |= (1<<REFS2)|(1<<REFS1)|(1<<MUX1)|(1<<MUX0);
+  ADCSRA |= (1<<ADEN)// Разрешение использования АЦП
+  |(1<<ADSC)//Запуск преобразования
+  |(1<<ADATE)//Непрерывный режим работы АЦП
+  |(1<<ADIE)//Разрешение прерываний
+  |(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);//Делитель 128 = 64 кГц
+  ADMUX |= (1<<REFS2)|(1<<REFS1)|(1<<REFS0)//Внутренний Источник ОН 2,56в и Vref PB0
+  |(1<<MUX1)|(1<<MUX0); // вход ADC3
+
   
 } 
 
-unsigned int ADC_convert (void) 
+/*unsigned int ADC_convert (void) 
 {
   //Начинаем преобразование
   ADCSRA |= (1<<ADSC); 
   //проверим закончилось ли преобразов
   while((ADCSRA & (1<<ADSC)));  
   return (unsigned int) ADC; 
-}
+}*/
 
 
 float n; 
@@ -31,8 +37,17 @@ void setup() {
 }
 
 void loop() {
- n =  (float) ADC_convert()/310;
- if(n >= 3.1 ) PORTB |= (1<<4);
+ n =  (float) adc_value/416;
+ if(n >= 1.76 ) PORTB |= (1<<4);
  else PORTB &= ~(1<<4);
 
+}
+
+
+ISR(ADC_vect)
+
+{
+  low_adc = ADCL;
+  high_adc = ADCH;
+  adc_value = high_adc*256+low_adc;
 }
